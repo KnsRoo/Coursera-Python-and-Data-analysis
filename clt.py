8753e60311e78435281b0e7d63af23f8be7680ee
KnsRoo <knsroo@gmail.com>
2018-02-28 20:01:44 +0300

    Update clt.py

--- clt.py
+++ clt.py
@@ -1,40 +1,32 @@
-import numpy as np
-import scipy.stats as sts
-import pandas as pd
-import matplotlib.pyplot as plt
-import math
+import math, numpy as np, scipy.stats as sts, pandas as pd, matplotlib.pyplot as plt
 
 def drawplot(x, mas, dis):
 	df = pd.DataFrame(mas, columns=['Плотность'])
 	ax = df.plot(kind='density')
 	plt.hist(mas, normed=True, label='Выборка')
 	plt.plot(x, dis.pdf(x),label='Теоретическая плотность')
-	plt.legend()
-	plt.ylabel('$f(x)$')
-	plt.xlabel('$x$')
+	plt.legend(); plt.ylabel('$f(x)$'); plt.xlabel('$x$')
 
-def generate(x, Mx, Sg, dist, dist_size, counts = 1000):
-	a = []
+def generate(x, Mx, Sg, dist, dist_size, out, counts = 1000, a = []):
 	for i in range(counts):
 		new_dist = dist[np.random.randint(0,len(dist),dist_size)]
 		a.append(np.mean(new_dist))
     
-	mu = np.mean(a)
-	sigma = np.std(a,ddof=1)
-	SE = Sg/math.sqrt(dist_size) # стандартная ошибка среднего
+	mu, sigma, SE = np.mean(a), np.std(a,ddof=1), Sg/math.sqrt(dist_size)
 	normal_dist = sts.norm(Mx,SE)
     
 	SE = round(SE,2); sigma = round(sigma,2); mu = round(mu,2)
-    
-	print("Теоретическое среднее "+str(Mx)+"\nПриближенное среднее "+str(mu))
-	print("Теоретическое ст. отклонение "+str(SE)+"\nПриближенное стандартное отклонение "+str(sigma))
-	print("Доверительный интервал "+str(mu)+" \033[4m+\033[0m "+str(2*SE))
 
 	drawplot(x, a, normal_dist)
+    
+	print(str(out)+" график:")
+	print("Теоретическое среднее vs Приближенное среднее: "+str(Mx)+" vs "+str(mu))
+	print("Теоретическое ст. отклонение vs Приближенное ст. отклонение: "+str(SE)+" vs "+str(sigma))
+	print("Доверительный интервал: "+str(Mx)+" \033[4m+\033[0m "+str(2*SE)); print('-'*30)   
 
 
 if __name__ == "__main__":
-	k = 3
+	k, i, lst = 3, 2, [5, 10, 50]
 	distrib = sts.chi2(k)
 	dist = distrib.rvs(1000)
 	x = np.linspace(0,20,100)
@@ -43,6 +35,5 @@ if __name__ == "__main__":
 
 	drawplot(x, dist, distrib)
 
-	lst = [5, 10, 50]
 	for item in lst:
-		generate(x, Mx, Sg, dist, item)
+		generate(x, Mx, Sg, dist, item, i); i+=1;
